ce2154bf3284d3c72daec0f8e6036c287dc66e8c
Tarannum Arshi <tarannum.arshi@PS97.Team>
2021-01-29 18:04:58 +0530

    Repository pattern

---
+++ SchoolManagement.Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SchoolManagement.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
