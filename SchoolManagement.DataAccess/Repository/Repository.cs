35ba48d25bdbf7372512a02e04dd34699648fd3b
Akash Deep <akash.deep@pulsesolutions.net>
2021-02-01 15:34:21 +0530

    fixrepo2

--- SchoolManagement.DataAccess/Repository/Repository.cs
+++ SchoolManagement.DataAccess/Repository/Repository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using SchoolManagement.DataAccess.Data.Repository.IRepository;
+using SchoolManagement.DataAccess.Data;
+using SchoolManagement.DataAccess.Repository.IRepository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- SchoolManagement.DataAccess/Repository/UnitOfWork.cs
+++ SchoolManagement.DataAccess/Repository/UnitOfWork.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SchoolManagement.DataAccess.Data;
+using SchoolManagement.DataAccess.Repository.IRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
