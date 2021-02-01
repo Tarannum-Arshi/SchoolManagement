5b4f7ffdd05593932e3c1536c67aa3934b66b2a6
Akash Deep <akash.deep@pulsesolutions.net>
2021-02-01 12:39:27 +0530

    FirstRepositoryCreated

---
+++ SchoolManagement.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagement.DataAccess.Data.Repository.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //ISPCall SPCall { get; }
+        void Save();
+    }
+}
---
+++ SchoolManagement.DataAccess/Data/Repository/IRepository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagement.DataAccess.Data.Repository.IRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            //UserRepository = new UserRepository(_db);
+            
+            //SPCall = new SPCall(_db);
+        }
+
+        //public IUserRepository UserRepository { get; private set; }
+       
+        //public ISPCall SPCall { get; private set; }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
