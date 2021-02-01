0ebf38794d6d0d16ba727b89ab198e4dbfa1cb66
Akash Deep <akash.deep@pulsesolutions.net>
2021-02-01 15:26:03 +0530

    fixrepo

---
+++ SchoolManagement.DataAccess/Repository/SPCall.cs
@@ -0,0 +1,52 @@
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
+using SchoolManagement.DataAccess.Data;
+using SchoolManagement.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolManagement.DataAccess.Repository
+{
+    public class SPCall : ISPCall
+    {
+        private readonly ApplicationDbContext _db;//for access of database
+        private static string ConnectionString = "";//whenever we have to call stored procedure we need connection string
+
+        public SPCall(ApplicationDbContext db)
+        {
+            _db = db;
+            ConnectionString = db.Database.GetDbConnection().ConnectionString;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(string procedureName, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> List<T>(string procedureName, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedureName, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T OneRecord<T>(string procedureName, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Single<T>(string procedureName, DynamicParameters param = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
