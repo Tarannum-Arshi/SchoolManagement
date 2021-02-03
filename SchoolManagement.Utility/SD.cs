5e5fed8e8c474d9ba914957e9a4454dddeedbf58
Akash Deep <akash.deep@pulsesolutions.net>
2021-02-03 10:59:18 +0530

    sdCahnge

--- SchoolManagement.Utility/SD.cs
+++ SchoolManagement.Utility/SD.cs
@@ -6,8 +6,8 @@ namespace SchoolManagement.Utility
 {
     public static class SD
     {
-        public const string Stud_Reg = "Registration";
-        public const string Teacher_Reg = "TeacherRegister";
+        public const string Stud_Reg = "registration";
+        public const string Teacher_Reg = "teacherRegister";
         public const string Drop = "getUserIdName";
         public const string GetFee = "getFeeOfStudent";
 
