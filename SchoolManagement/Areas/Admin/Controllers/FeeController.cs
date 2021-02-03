1d5607b86a6eb20f97c18cbd30b31e8af16277f6
Tarannum Arshi <tarannum.arshi@PS97.Team>
2021-02-03 15:09:18 +0530

    ui

---
+++ SchoolManagement/Areas/Admin/Controllers/FeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolManagement.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolManagement.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FeeController : Controller
+    {
+        public readonly IUnitOfWork _unitOfWork;
+
+        public FeeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
